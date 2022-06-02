cd5cccb08451c4fa243f813bdb21e4a4eb2f12db
Nadine Rodriguez <inadinerod@gmail.com>
2022-06-01 20:31:09 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var instagram = document.querySelector(".fa-instagram");
+instagram.style.color = "#d45bb9";
+
+var facebook = document.querySelector(".fa-facebook");
+facebook.style.color = "#6a99b5";
+
+var hours = document.querySelector(".contact p");
+// console.log(hours);
+hours.style.border = "1px solid #d45bb9";
+hours.style.padding = "1em";
